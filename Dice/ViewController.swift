f47a9128b5fab85e817e30cfc76723faa27de73f
João Victor <jvctor23@gmail.com>
2022-03-14 23:53:15 -0300

    Logic

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var diceRight: UIImageView!
+    @IBOutlet weak var diceLeft: UIImageView!
+    
+    let diceArray = ["DiceOne", "DiceTwo", "DiceThree", "DiceFour", "DiceFive", "DiceSix"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func btnRollPressed(_ sender: Any) {
+        diceRight.image = UIImage(named: diceArray[Int.random(in: 0...5)])
+        diceLeft.image = UIImage(named: diceArray[Int.random(in: 0...5)])
+    }
+    
 
 }
 
